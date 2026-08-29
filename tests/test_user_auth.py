bcd0e5b473168fadfb4bb6402ac25992c1ce616a
Bereket Zeselassie <bereketzeselassie@gmail.com>
2026-08-29 19:06:48 +0300

    test: add automated unit test for issue #30

---
+++ tests/test_user_auth.py
@@ -0,0 +1,12 @@
+from datetime import datetime, timedelta
+from backend.utils.user_auth import is_session_expired
+
+
+def test_is_session_expired_fresh_token():
+    fresh_time = (datetime.now() - timedelta(seconds=10)).isoformat()
+    assert is_session_expired(fresh_time, max_age_seconds=3600) is False
+
+
+def test_is_session_expired_old_token():
+    old_time = (datetime.now() - timedelta(seconds=4000)).isoformat()
+    assert is_session_expired(old_time, max_age_seconds=3600) is True
