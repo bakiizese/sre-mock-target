9b80b0e0098d341f7b28d698295786ff7a84ae3b
Bereket Zeselassie <bereketzeselassie@gmail.com>
2026-08-30 00:03:53 +0300

    test: add automated unit test for issue #41

--- tests/test_pricing.py
+++ tests/test_pricing.py
@@ -1,8 +1,8 @@
 import pytest
 from app.utils.pricing import calculate_discount
 
-def test_calculate_discount_with_none_price():
+def test_calculate_discount_with_none():
     assert calculate_discount(None, 0.1) == 0.0
 
-def test_calculate_discount_valid():
-    assert calculate_discount(100.0, 0.1) == 90.0
+def test_calculate_discount_normal():
+    assert calculate_discount(100.0, 0.2) == 80.0
