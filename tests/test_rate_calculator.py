89e2c93ab2ac879b529b44aa732a14d1a32aebe5
Bereket Zeselassie <bereketzeselassie@gmail.com>
2026-08-29 12:09:07 +0300

    test: add automated unit test for issue #1

--- tests/test_rate_calculator.py
+++ tests/test_rate_calculator.py
@@ -1,8 +1,8 @@
 import pytest
 from rate_calculator import calculate_rate
 
-def test_calculate_rate_normal():
+def test_calculate_rate_success():
     assert calculate_rate(10, 2) == 5.0
 
-def test_calculate_rate_zero_count():
+def test_calculate_rate_zero_division():
     assert calculate_rate(10, 0) == 0.0
